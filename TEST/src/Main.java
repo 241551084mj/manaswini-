952d0987fc75ef8b27ca11ea6fa9e5f5815ed896
241551084mj <241551084@kiit.ac.in>
2025-12-11 16:56:19 +0530

    initial commit for lab1 q1

---
+++ TEST/src/Main.java
@@ -0,0 +1,13 @@
+class StudentInfo {
+    public static void main(String[] args) {
+        String name = "Manaswini";
+        String roll = "123456";
+        String section = "CSE-2";
+        String branch = "CSE";
+
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + roll);
+        System.out.println("Section: " + section);
+        System.out.println("Branch: " + branch);
+    }
+}
